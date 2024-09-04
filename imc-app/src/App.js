a6d47eaf68973bbcc33a429c029edb31a5a37d9c
skar_penelope <penelopeskarlate@gmail.com>
2024-09-04 15:42:39 -0300

    inicio

---
+++ imc-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <main>
+      <div className="App">
+      <div className="quadrado">
+        <fieldset>
+          <div>
+            <h4>Peso</h4>
+            <input type="number" name="valor" id="peso"></input>
+            <h4>Altura</h4>
+            <input type="numer" name="valorAlt" id="altura"></input>
+          </div>
+        </fieldset>
+
+      </div>
+    </div>
+
+    </main>
+  
+  );
+}
+
+
+export default App;
