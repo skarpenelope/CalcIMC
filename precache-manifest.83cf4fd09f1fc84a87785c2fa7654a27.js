2e8a985dc852c32e65f8ac9a806bb03d30b624cb
skar_penelope <penelopeskarlate@gmail.com>
2024-09-27 16:33:33 -0300

    Updates

---
+++ precache-manifest.83cf4fd09f1fc84a87785c2fa7654a27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cd7fba4fdb59a47d660ae344e65668",
+    "url": "/CalcIMC/index.html"
+  },
+  {
+    "revision": "7a3dceb4b7fab82c40ff",
+    "url": "/CalcIMC/static/css/main.d63585d3.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/CalcIMC/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/CalcIMC/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "7a3dceb4b7fab82c40ff",
+    "url": "/CalcIMC/static/js/main.3ec589b8.chunk.js"
+  },
+  {
+    "revision": "caf48385ebd4b1843955",
+    "url": "/CalcIMC/static/js/runtime~main.c12e6572.js"
+  },
+  {
+    "revision": "6d12dd5ac494df5a0ae4f259ad795963",
+    "url": "/CalcIMC/static/media/fitimg.6d12dd5a.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/CalcIMC/static/media/logo.06e73328.svg"
+  }
+]);
